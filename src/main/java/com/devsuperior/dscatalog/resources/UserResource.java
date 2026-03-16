3231266e84ea4c4ecff48484b73b6ff281e5d859
Nathan Soares <nathansoares@email.com>
2026-03-16 13:19:35 -0300

    UserUpdateValidator

--- src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.resources;
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.services.UserService;
 import jakarta.validation.Valid;
@@ -48,7 +49,7 @@ public class UserResource {
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody @Valid UserDTO dto) {
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO dto) {
 		UserDTO newDto = service.update(id, dto);
 		return ResponseEntity.ok().body(newDto);
 	}
